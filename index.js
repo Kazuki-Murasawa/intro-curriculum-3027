686c95db46002c299f76bd0f8d3d5cbe50d2637b
Kazuki-Murasawa <sunburst47@gmail.com>
2019-02-05 09:26:06 +0000

    execSyncを使わない

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 'use strict';
 const http = require('http');
-const cp = require('child_process');
+//const cp = require('child_process');
 const server = http.createServer((req, res) => {
   const path = req.url;
-  res.end(cp.execSync('echo ' + path));
+  //res.end(cp.execSync('echo ' + path));
+  res.end(path);
 });
 const port = 8000;
 server.listen(port, () => {
   console.info('Listening on ' + port);
 });
+//execSync のような OS コマンドを実行する関数はなるべく使わないことが望ましいです。
+//どうしても必要で使用する場合には、細心の注意が必要です。
